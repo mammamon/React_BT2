f198af59ee12058e078a2035bc0ef3806c6a4dca
mammamon <quangnguyenonlinevn@gmail.com>
2023-07-30 03:03:45 +0700

    v30070303

--- src/OnlineGlassesComponents/Product.jsx
+++ src/OnlineGlassesComponents/Product.jsx
@@ -1,51 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 const Product = () => {
+  const [dataGlasses, setDataGlasses] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  useEffect(() => {
+    fetch('dataGlasses.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        setDataGlasses(data);
+        // mặc định load data thứ 1
+        setSelectedProduct(data[0]);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+  }
+
   return (
     <div className='product-zone container d-flex justify-content-center flex-wrap mt-5'>
-        <div className="product-model d-flex justify-content-between">
-            <div className="product-model-img">
-                <img src='glassesImage/model.jpg'/>
-                <img src='glassesImage/v9.png' id="product-wear"/>
-                <div className="product-info p-1">
-                    <h3 id="product-name">fdsfdfdsf</h3>
-                    <p id="product-desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, dolorem suscipit? Fugiat saepe nemo ut!</p>
-                </div>
-            </div>
-            <div className="product-model-img">
-                <img src='glassesImage/model.jpg'/>
-            </div>
+      <div className="product-model d-flex justify-content-between">
+        <div className="product-model-img">
+          <img src='glassesImage/model.jpg' alt="đeo kính" />
+          {selectedProduct && <img src={selectedProduct.url} id="product-wear" />}
+          <div className="product-info p-1">
+            {selectedProduct && (
+              <>
+                <h3 id="product-name">{selectedProduct.name}</h3>
+                <p id="product-desc">{selectedProduct.desc}</p>
+              </>
+            )}
+          </div>
         </div>
-        <div className="product-show d-flex flex-wrap align-items-center justify-content-center">
-            <div className="product">
-                <img src='glassesImage/g1.jpg' alt="" />
-            </div>
-            <div className="product">
-                <img src='glassesImage/g2.jpg' alt="" />
-            </div>
-            <div className="product">
-                <img src='glassesImage/g3.jpg' alt="" />
-            </div>
-            <div className="product">
-                <img src='glassesImage/g4.jpg' alt="" />
-            </div>
-            <div className="product">
-                <img src='glassesImage/g5.jpg' alt="" />
-            </div>
-            <div className="product">
-                <img src='glassesImage/g6.jpg' alt="" />
-            </div>
-            <div className="product">
-                <img src='glassesImage/g7.jpg' alt="" />
-            </div>
-            <div className="product">
-                <img src='glassesImage/g8.jpg' alt="" />
-            </div>
-            <div className="product">
-                <img src='glassesImage/g9.jpg' alt="" />
-            </div>
+        <div className="product-model-img">
+          <img src='glassesImage/model.jpg' alt="đeo kính" />
         </div>
+      </div>
+      <div className="product-show d-flex flex-wrap align-items-center justify-content-center">
+        {dataGlasses.map((product, index) => (
+          <div key={index} className="product" onClick={() => handleProductClick(product)}>
+            <img src={`glassesImage/g${index + 1}.jpg`} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default Product
+export default Product;
